27cddf2267eec94a5f8a85120ed2d22756a29e51
ardonik <abuse@ardonik.org>
2017-08-17 12:22:52 -0700

    Minor header file update.  Compilation is unaffected.
    - include/ComputerPlayer.h: Those member variables for class ComputerPlayer
      ought to be private.  It's play() and remember() that are the override
      functions of interest, so I put them in a separate public section.

--- robotics/rock-paper-scissors-exercise/include/ComputerPlayer.h
+++ robotics/rock-paper-scissors-exercise/include/ComputerPlayer.h
@@ -13,14 +13,18 @@ class ComputerPlayer : public PlayerBase {
         ComputerPlayer();
         ~ComputerPlayer() { }
         std::string name() const;
-        void remember(const string& myLastPlay, const string& theirLastPlay);
 
     public:
 
+        void remember(const string& myLastPlay, const string& theirLastPlay);
         std::string play();
+
+    private:
+
         static int counter;
         int id_;
         string name_;
+
 };
 
 #endif // #ifndef COMPUTER_PLAYER_H__
