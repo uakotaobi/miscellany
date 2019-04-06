74e9580754feb80a20157c49bb20cf0e31643067
uakotaobi <uche.robotics@akotaobi.org>
2019-04-06 16:04:45 -0700

    Add simple changes that allow me to control the V5 mentor-bot with the analog sticks.
    This allows you to drive hte mentor-bot!  But it's way too fast, and is
    really quite unusable.

--- robotics/pros-hello-world/src/opcontrol.cpp
+++ robotics/pros-hello-world/src/opcontrol.cpp
@@ -15,17 +15,24 @@
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
+	pros::Motor back_left_mtr(1);
+	pros::Motor back_right_mtr(2);
+	pros::Motor front_right_mtr(10);
+	pros::Motor front_left_mtr(9);
+	pros::Motor myMotor(4);
 	while (true) {
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
+		int left = -master.get_analog(ANALOG_LEFT_Y);
 		int right = master.get_analog(ANALOG_RIGHT_Y);
 
-		left_mtr = left;
-		right_mtr = right;
+		back_left_mtr = left;
+		back_right_mtr = right;
+		front_left_mtr = left;
+		front_right_mtr = right;
+		//myMotor.move(120);
+
 		pros::delay(20);
 	}
 }
